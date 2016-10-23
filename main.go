b083e48f0c4bdde9d3fa175352895c1d0a6dee27
Igor Pohasii <aroundofyou@gmail.com>
2016-10-23 21:19:44 +0200

    Add New Post form

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ import (
   "html/template"
 )
 
+func writeHandler(w http.ResponseWriter, r *http.Request) {
+  t, err := template.ParseFiles(
+    "templates/write.html",
+    "templates/header.html",
+    "templates/footer.html")
+  if err != nil {
+    fmt.Println(w, err.Error())
+  }
+  t.ExecuteTemplate(w, "write", nil)
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
   t, err := template.ParseFiles(
     "templates/index.html",
@@ -23,6 +34,7 @@ func main() {
 
   http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
   http.HandleFunc("/", indexHandler)
+  http.HandleFunc("/write", writeHandler)
 
   err := http.ListenAndServe(":3000", nil)
   if err != nil {
